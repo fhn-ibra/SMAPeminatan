08a95b7c3a42620e7d683c7167c153810d7b106b
Ibra <farhanibra21@gmail.com>
2024-03-28 20:59:32 +0700

    Update Data Seeder

--- database/seeders/DataSeeder.php
+++ database/seeders/DataSeeder.php
@@ -29,91 +29,91 @@ class DataSeeder extends Seeder
             [
                 'id' => 111,
                 'nama_paket' => 'SAINTEK A',
-                'nama_kelas' => 'XI IPA 1',
+                'nama_kelas' => 'XI-IPA 1',
                 'stok' => 40,
                 'jurusan_id' => '100'
             ], [
                 'id' => 112,
                 'nama_paket' => 'SAINTEK A',
-                'nama_kelas' => 'XI IPA 2',
+                'nama_kelas' => 'XI-IPA 2',
                 'stok' => 40,
                 'jurusan_id' => '100'
             ], [
                 'id' => 113,
                 'nama_paket' => 'SAINTEK A',
-                'nama_kelas' => 'XI IPA 3',
+                'nama_kelas' => 'XI-IPA 3',
                 'stok' => 40,
                 'jurusan_id' => '100'
             ], [
                 'id' => 114,
                 'nama_paket' => 'SAINTEK A',
-                'nama_kelas' => 'XI IPA 4',
+                'nama_kelas' => 'XI-IPA 4',
                 'stok' => 40,
                 'jurusan_id' => '100'
             ], [
                 'id' => 121,
                 'nama_paket' => 'SAINTEK B',
-                'nama_kelas' => 'XI IPA 5',
+                'nama_kelas' => 'XI-IPA 5',
                 'stok' => 40,
                 'jurusan_id' => '100'
             ], [
                 'id' => 122,
                 'nama_paket' => 'SAINTEK B',
-                'nama_kelas' => 'XI IPA 6',
+                'nama_kelas' => 'XI-IPA 6',
                 'stok' => 40,
                 'jurusan_id' => '100'
             ], [
                 'id' => 131,
                 'nama_paket' => 'SAINTEK C',
-                'nama_kelas' => 'XI IPA 7',
+                'nama_kelas' => 'XI-IPA 7',
                 'stok' => 40,
                 'jurusan_id' => '100'
             ], [
                 'id' => 141,
                 'nama_paket' => 'SAINTEK D',
-                'nama_kelas' => 'XI IPA 8',
+                'nama_kelas' => 'XI-IPA 8',
                 'stok' => 40,
                 'jurusan_id' => '100'
             ], [
                 'id' => 211,
                 'nama_paket' => 'SOSHUM E',
-                'nama_kelas' => 'XI IPS 1',
+                'nama_kelas' => 'XI-IPS 1',
                 'stok' => 40,
                 'jurusan_id' => '200'
             ], [
                 'id' => 212,
                 'nama_paket' => 'SOSHUM E',
-                'nama_kelas' => 'XI IPS 2',
+                'nama_kelas' => 'XI-IPS 2',
                 'stok' => 40,
                 'jurusan_id' => '200'
             ], [
                 'id' => 213,
                 'nama_paket' => 'SOSHUM E',
-                'nama_kelas' => 'XI IPS 3',
+                'nama_kelas' => 'XI-IPS 3',
                 'stok' => 40,
                 'jurusan_id' => '200'
             ], [
                 'id' => 214,
                 'nama_paket' => 'SOSHUM E',
-                'nama_kelas' => 'XI IPS 4',
+                'nama_kelas' => 'XI-IPS 4',
                 'stok' => 40,
                 'jurusan_id' => '200'
             ], [
                 'id' => 221,
                 'nama_paket' => 'SOSHUM F',
-                'nama_kelas' => 'XI IPS 5',
+                'nama_kelas' => 'XI-IPS 5',
                 'stok' => 40,
                 'jurusan_id' => '200'
             ], [
                 'id' => 222,
                 'nama_paket' => 'SOSHUM F',
-                'nama_kelas' => 'XI IPS 6',
+                'nama_kelas' => 'XI-IPS 6',
                 'stok' => 40,
                 'jurusan_id' => '200'
             ], [
                 'id' => 231,
                 'nama_paket' => 'SOSHUM G',
-                'nama_kelas' => 'XI IPS 7',
+                'nama_kelas' => 'XI-IPS 7',
                 'stok' => 40,
                 'jurusan_id' => '200'
             ]];
