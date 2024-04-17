925e2f12574021a81b972e09bf199648f3deeb78
Ibra <farhanibra21@gmail.com>
2024-04-17 22:11:10 +0700

    Add Comment

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
+//Ini buat Ngambil seluruh data pendaftar, itu gw tadi tambahin middleware sanctum buat coba coba. Nanti hapus aja
 Route::get('/pendaftar', [ApiController::class, 'pendaftar'])->middleware('auth:sanctum');
+
+//Ini buat ngambil data seluruh kelas
 Route::get('/kelas', [ApiController::class, 'kelas']);
+
+//Ini buat ngambil data Siswa sesuai Kelasnya contoh : /data/111
 Route::get('/data/{id}', [ApiController::class, 'dataKelas']);
 
+//ini sih sebenernya delete data dari table Jurusan
 Route::get('/delete/{id}', [ApiController::class, 'delete']);
 
+//Ini buat nambahin data dari table Jurusan
 Route::post('/insert', [ApiController::class, 'insert']);
